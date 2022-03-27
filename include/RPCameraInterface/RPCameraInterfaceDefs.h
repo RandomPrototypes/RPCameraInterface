84892018f8435038ce1f76fbba41d51b9fd6fcd6
RandomPrototypes <delfare1-randomprototypes@yahoo.com>
2022-03-27 14:48:37 +0900

    add dllimport

--- include/RPCameraInterface/RPCameraInterfaceDefs.h
+++ include/RPCameraInterface/RPCameraInterfaceDefs.h
@@ -4,8 +4,12 @@
 //Taken from OpenCV
 
 #ifndef RPCAM_EXPORTS
-# if (defined _WIN32 || defined WINCE || defined __CYGWIN__) && defined(RPCAMERAINTERFACE_EXPORTS)
-#   define RPCAM_EXPORTS __declspec(dllexport)
+# if (defined _WIN32 || defined WINCE || defined __CYGWIN__)
+#   if defined(RPCAMERAINTERFACE_EXPORTS)
+#      define RPCAM_EXPORTS __declspec(dllexport)
+#   else
+#      define RPCAM_EXPORTS __declspec(dllimport)
+#   endif
 # elif defined __GNUC__ && __GNUC__ >= 4 && (defined(RPCAMERAINTERFACE_EXPORTS) || defined(__APPLE__))
 #   define RPCAM_EXPORTS __attribute__ ((visibility ("default")))
 # endif
